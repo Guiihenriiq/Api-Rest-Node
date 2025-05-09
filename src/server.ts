f13437f81e76df87d0bddaa0d378a71523b949df
Guiihenriiq <henri3985@gmail.com>
2025-05-08 20:22:29 -0400

    Banco de dados - Realizando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import fastify from "fastify";
+import crypto from 'node:crypto'
 import { knex } from "./database"; // use the configured instance
 
 const app = fastify();
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*');
-    return tables;
+    const transactions = await knex('transactions')
+    .where('amount',1000)
+    .select('*')
+
+    return transactions
 });
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP Server Running!');
+    console.log('db Is Running');
 });
