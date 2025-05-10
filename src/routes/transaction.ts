15e0ecd0ab564d6381176f9bb9501fb163f6e752
Guiihenriiq <henri3985@gmail.com>
2025-05-09 20:56:55 -0400

    Implementando rotas - Criação de transações

---
+++ src/routes/transaction.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
+import { knex } from "../database";
+
+
+export function transactionsRoutes(app:FastifyInstance){
+    app.post('/', async (request,reply) => {
+        const createTransectionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit']),
+        }) 
+
+        const {title,amount,type} = createTransectionBodySchema.parse(
+            request.body)
+
+            await knex('transactions')
+            .insert({
+                id: randomUUID(),
+                title,
+                amount: type === 'credit' ? amount : amount *-1,
+            })
+
+        return reply.status(201).send("Transaction Vary GoodS")
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import fastify from "fastify";
 import { env } from "./env";
-import { knex } from "./database"; 
-
+import { transactionsRoutes } from "./routes/transaction";
 
 const app = fastify();
 
-app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-    .where('amount',1000)
-    .select('*')
-
-    return transactions
-});
+app.register(transactionsRoutes,{
+    prefix:'transactions',
+})
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP Server Running!');
