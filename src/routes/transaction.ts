8efe80b8c703a5da616f076ff9d9936844620f0b
Guiihenriiq <henri3985@gmail.com>
2025-05-09 21:24:43 -0400

    Implementando rotas - Listagem das transações

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -6,6 +6,26 @@ import { Tables } from "knex/types/tables";
 
 
 export function transactionsRoutes(app:FastifyInstance){
+    app.get('/', async() => {
+        const transactions = await knex('transactions')
+        .select()
+
+        return {transactions}
+    })
+
+    app.get('/:id', async(request) => {
+        const getTransactionsParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionsParamsSchema.parse(request.params)
+
+        const transaction = await knex('transactions')
+        .where('id',id).first()
+
+        return {transaction}
+    })
+
     app.post('/', async (request,reply) => {
         const createTransectionBodySchema = z.object({
             title: z.string(),
