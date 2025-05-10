bf4f7cadc160e50703dc66902f7bd0267207be71
Guiihenriiq <henri3985@gmail.com>
2025-05-09 21:09:57 -0400

    Implementando rotas - Tipagem no Knex

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { randomUUID } from "node:crypto";
 import { knex } from "../database";
+import { Tables } from "knex/types/tables";
 
 
 export function transactionsRoutes(app:FastifyInstance){
